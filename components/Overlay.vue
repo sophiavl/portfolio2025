8d931dd6d094552f99dedee064afa74c8a21eb79
sophiavl <102966872+sophiavl@users.noreply.github.com>
2025-11-13 18:42:38 +0100

    open and close overlay

--- components/Overlay.vue
+++ components/Overlay.vue
@@ -3,8 +3,20 @@
     class="w-screen h-screen absolute bg-black/75 flex flex-col justify-center items-center"
   >
     <section
-      class="w-[80%] h-[80%] bg-[#FFF9EB] top-30 m-auto rounded-4xl flex flex-col justify-start items-center p-6"
+      class="relative w-[80%] h-[80%] bg-[#FFF9EB] top-30 rounded-4xl flex flex-col justify-start items-center pt-12 p-[3rem] md:p-14"
     >
+      <button
+        @click="onClick"
+        class="absolute right-5 top-5 text-[#120000]"
+      >
+        <Icon
+          icon="material-symbols-light:close"
+          width="36"
+          height="36"
+          style="color: #120000"
+        />
+      </button>
+
       <section class="flex flex-col w-full items-center pb-4">
         <h1 class="text-[#00131B]">1 december</h1>
         <div class="w-[80%] h-[0.1rem] bg-[#c3c3c3]"></div>
@@ -35,6 +47,13 @@
 </template>
 
 <script setup>
+import { defineEmits } from "vue";
+import { Icon } from "@iconify/vue";
+const emit = defineEmits(["closeOverlay"]);
+
+function onClick() {
+  emit("closeOverlay");
+}
 const p1 =
   "Welkom bij de eerste dag van mijn `Internet Blender Adventkalender`!";
 const p2 =
@@ -44,3 +63,64 @@ const p3 =
 const p4 =
   "Maar dat zullen jullie vanzelf wel merken, als je elke dag terugkomt natuurlijk :) De eerste dag beginnen we met deze geweldige website, sorry voor de mensen met thalassofobie (oeps, spoiler).";
 </script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+h1 {
+  font-family: "InstrumentSerif";
+  font-size: 1.9rem;
+  font-weight: bold;
+  letter-spacing: 0.17rem;
+}
+
+h2 {
+  font-size: 1.1rem;
+  font-family: "CothamSans";
+  font-weight: normal;
+}
+h3 {
+  font-size: 0.9rem;
+  font-weight: normal;
+  font-family: "CothamSans";
+}
+
+p {
+  font-family: "CothamSans";
+  font-size: 0.9rem;
+  color: #00131b;
+  text-align: center;
+}
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+  padding: 3px;
+}
+
+@media only screen and (min-width: 490px) {
+  :deep(p) {
+    font-size: 1rem;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  :deep(p) {
+    font-size: 0.8rem;
+  }
+}
+@media only screen and (min-width: 900px) {
+  :deep(p) {
+    font-size: 1rem;
+  }
+}
+
+@media only screen and (min-width: 1024px) {
+  :deep(p) {
+    font-size: 1.2rem !important;
+  }
+}
+</style>
--- pages/adventkalender.vue
+++ pages/adventkalender.vue
@@ -2,34 +2,38 @@
   <main
     class="flex flex-col h-screen justify-between items-center overflow-hidden"
   >
-    <Overlay v-if="overlay"></Overlay>
-    <section class="h-[98%] w-screen border-black">
+    <Overlay
+      v-if="overlay"
+      @closeOverlay="overlay = false"
+    ></Overlay>
+    <section class="h-[97%] w-screen border-black">
       <div class="flex h-[10%] w-screen">
-        <div
-          class="w-1/3 h-full flex justify-center text-[#FEF9E1] items-center text-xl bg-[#A31D1D]"
+        <button
+          @click="overlay = !overlay"
+          class="w-1/3 h-full flex justify-center text-[#FEF9E1] items-center text-xl bg-[#A31D1D] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           1
-        </div>
+        </button>
         <div
-          class="w-1/3 h-full flex justify-center text-[#FEF9E1] items-center text-xl bg-[#3B7B61]"
+          class="w-1/3 h-full flex justify-center text-[#FEF9E1] items-center text-xl bg-[#3B7B61] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           2
         </div>
         <div
-          class="w-1/3 h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#551C22]"
+          class="w-1/3 h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#551C22] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           3
         </div>
       </div>
       <div class="flex h-[20%] w-screen">
         <div
-          class="w-[20%] h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#1B4D3E] xl:w-[27.5%]"
+          class="w-[20%] h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#1B4D3E] xl:w-[27.5%] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           4
         </div>
         <header
           v-if="!overlay"
-          class="relative flex flex-col h-full bg-[#FEF9E1] w-[60%] pt-6 justify-start items-center md:pl-12 xl:w-[45%]"
+          class="relative flex flex-col h-full bg-[#FEF9E1] w-[60%] pl-6 pt-6 justify-start items-center md:pl-12 xl:w-[45%]"
         >
           <section class="flex flex-col items-start">
             <h2 class="text-[#120000] self-start">De Internet Blender</h2>
@@ -39,91 +43,91 @@
             </h3>
           </section>
           <img
-            class="absolute bottom-0 left-[4%] w-14 h-auto md:w-24 lg:left-[10%]"
+            class="absolute bottom-0 left-[1%] w-12 h-auto md:w-28 lg:left-[10%]"
             src="/public/images/boompje.png"
             alt="kerstboompje"
           />
         </header>
         <div
-          class="w-[20%] h-full flex justify-center text-[#FEF9E1] items-center text-xl bg-[#3B7B61] xl:w-[27.5%]"
+          class="w-[20%] h-full flex justify-center text-[#FEF9E1] items-center text-xl bg-[#3B7B61] xl:w-[27.5%] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           5
         </div>
       </div>
       <div class="flex h-[10%] w-screen">
         <div
-          class="w-1/2 h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#551C22]"
+          class="w-1/2 h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#551C22] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           6
         </div>
         <div
-          class="w-1/2 h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#1B4D3E]"
+          class="w-1/2 h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#1B4D3E] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           7
         </div>
       </div>
       <div class="flex h-[10%] w-screen">
         <div
-          class="w-[15%] h-full flex justify-center items-center text-[#120000] text-xl bg-[#FEF9E1]"
+          class="w-[15%] h-full flex justify-center items-center text-[#120000] text-xl bg-[#FEF9E1] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           8
         </div>
         <div class="flex flex-col h-full w-[45%]">
           <div
-            class="flex justify-center items-center text-[#FEF9E1] text-xl h-1/2 bg-[#A31D1D]"
+            class="flex justify-center items-center text-[#FEF9E1] text-xl h-1/2 bg-[#A31D1D] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
           >
             9
           </div>
           <div
-            class="flex justify-center items-center text-[#FEF9E1] text-xl h-1/2 bg-[#1B4D3E]"
+            class="flex justify-center items-center text-[#FEF9E1] text-xl h-1/2 bg-[#1B4D3E] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
           >
             10
           </div>
         </div>
         <div
-          class="w-[40%] h-full flex justify-center text-[#120000] items-center text-xl bg-[#FEF9E1]"
+          class="w-[40%] h-full flex justify-center text-[#120000] items-center text-xl bg-[#FEF9E1] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           11
         </div>
       </div>
-      <div class="flex h-[8%] w-screen">
+      <div class="flex h-[9%] w-screen">
         <div
-          class="h-full w-1/2 flex justify-center items-center text-[#FEF9E1] text-xl bg-[#A31D1D]"
+          class="h-full w-1/2 flex justify-center items-center text-[#FEF9E1] text-xl bg-[#A31D1D] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           12
         </div>
         <div
-          class="w-1/4 flex justify-center items-center text-[#FEF9E1] text-xl bg-[#3B7B61]"
+          class="w-1/4 flex justify-center items-center text-[#FEF9E1] text-xl bg-[#3B7B61] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           13
         </div>
 
         <div
-          class="w-1/4 h-full flex justify-center text-[#FEF9E1] items-center text-xl bg-[#1B4D3E]"
+          class="w-1/4 h-full flex justify-center text-[#FEF9E1] items-center text-xl bg-[#1B4D3E] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           14
         </div>
       </div>
       <div class="flex h-[21%] w-screen">
         <div
-          class="w-3/5 h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#551C22]"
+          class="w-3/5 h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#551C22] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           15
         </div>
         <div class="flex flex-col w-1/4">
           <div
-            class="h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#A31D1D]"
+            class="h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#A31D1D] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
           >
             16
           </div>
           <div
-            class="h-1/2 flex justify-center items-center text-[#FEF9E1] text-xl bg-[#3B7B61]"
+            class="h-1/2 flex justify-center items-center text-[#FEF9E1] text-xl bg-[#3B7B61] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
           >
             17
           </div>
         </div>
         <div
-          class="w-1/5 h-full flex justify-center items-center text-[#120000] text-xl bg-[#FEF9E1]"
+          class="w-1/5 h-full flex justify-center items-center text-[#120000] text-xl bg-[#FEF9E1] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           18
         </div>
@@ -131,42 +135,44 @@
       <div class="flex h-[10%] w-screen">
         <div class="flex flex-col h-full w-1/3">
           <div
-            class="h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#1B4D3E]"
+            class="h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#1B4D3E] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
           >
             19
           </div>
           <div
-            class="h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#551C22]"
+            class="h-full flex justify-center items-center text-[#FEF9E1] text-xl bg-[#551C22] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
           >
             20
           </div>
         </div>
         <div
-          class="h-full w-1/3 flex justify-center text-[#120000] items-center text-xl bg-[#FEF9E1]"
+          class="h-full w-1/3 flex justify-center text-[#120000] items-center text-xl bg-[#FEF9E1] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           21
         </div>
         <div
-          class="h-full w-1/3 flex justify-center items-center text-xl bg-[#551C22]"
+          class="h-full w-1/3 flex justify-center items-center text-xl bg-[#551C22] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           22
         </div>
       </div>
       <div class="flex h-[10%] w-screen">
         <div
-          class="h-full w-1/2 flex justify-center items-center text-[#FEF9E1] text-xl bg-[#A31D1D]"
+          class="h-full w-1/2 flex justify-center items-center text-[#FEF9E1] text-xl bg-[#A31D1D] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           23
         </div>
         <div
-          class="h-full w-1/2 flex justify-center items-center text-[#FEF9E1] text-xl bg-[#3B7B61]"
+          class="h-full w-1/2 flex justify-center items-center text-[#FEF9E1] text-xl bg-[#3B7B61] shadow-[inset_0_0_7px_1px_rgba(0,0,0,0.25)]"
         >
           24
         </div>
       </div>
     </section>
-    <footer class="flex h-[2%] items-center">
-      <p class="pl-4 text-[#FFF9EB]">Made with ❤️ by Sophia</p>
+    <footer
+      class="flex h-[3%] w-screen justify-center items-center bg-[#FEF9E1]"
+    >
+      <p class="pl-4 !text-[0.7rem] text-[#FFF9EB]">Made with ❤️ by Sophia</p>
       <a href="https://www.sophiavanlieshout.nl"
         ><img
           src="../public/images/bloemetje.svg"
@@ -179,7 +185,9 @@
 </template>
 <script setup>
 import Overlay from "../components/Overlay.vue";
-const overlay = false;
+import { ref } from "vue";
+
+const overlay = ref(false);
 </script>
 
 <style scoped>
@@ -189,7 +197,7 @@ header {
 
 h1 {
   font-family: "InstrumentSerif";
-  font-size: 1.8rem;
+  font-size: 1.9rem;
   font-weight: bold;
   letter-spacing: 0.17rem;
 }
@@ -219,36 +227,53 @@ a,
   padding: 3px;
 }
 
-@media only screen and (min-width: 768px) {
-  h1 {
-    font-size: 2.8rem;
+@media only screen and (min-width: 490px) {
+  :deep(h1) {
+    font-size: 2.3rem;
   }
 
-  h2 {
+  :deep(h2) {
     font-size: 1.4rem;
   }
-  h3 {
+  :deep(h3) {
     font-size: 1.1rem;
   }
 
-  p {
+  :deep(p) {
+    font-size: 0.8rem;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  :deep(h1) {
+    font-size: 2.8rem;
+  }
+
+  :deep(h2) {
+    font-size: 1.4rem;
+  }
+  :deep(h3) {
+    font-size: 1.1rem;
+  }
+
+  :deep(p) {
     font-size: 0.8rem;
   }
 }
 
 @media only screen and (min-width: 1024px) {
-  h1 {
+  :deep(h1) {
     font-size: 3rem;
   }
 
-  h2 {
+  :deep(h2) {
     font-size: 1.4rem;
   }
-  h3 {
+  :deep(h3) {
     font-size: 1.1rem;
   }
 
-  p {
+  :deep(p) {
     font-size: 0.8rem;
   }
 }
